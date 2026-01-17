b5aed88a4ea77ed18d17b25feda3ea09176230c9
Andrey Korotkow <andreyrule@gmail.com>
2026-01-17 12:15:38 +0300

    add navigations

---
+++ src/navigation/MainNavigation.tsx
@@ -0,0 +1,17 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '@/screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+export function MainNavigation() {
+  return (
+    <Stack.Navigator
+      initialRouteName='Home'
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name='Home' component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
